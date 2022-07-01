1d84be9bd95620cced6abe173aa4f0686b9d13c0
BOBOLA <w0772193@myscc.ca>
2022-06-30 21:24:50 -0400

    added routing and backend components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import {ProductsComponent} from "./components/products/products.component";
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { StayConnectedComponent } from './components/stay-connected/stay-connected.component';
-import { TutorialComponent } from './components/tutorial/tutorial.component';
-
+import {ProductsComponent} from "./Components/home/home-page/products/products.component";
+import { FooterComponent } from './Components/home/home-page/footer/footer.component';
+import { HeaderComponent } from './Components/home/home-page/header/header.component';
+import { StayConnectedComponent } from './Components/home/home-page/stay-connected/stay-connected.component';
+import { TutorialComponent } from './Components/home/home-page/tutorial/tutorial.component';
+import { NavigationComponent } from './Components/home/home-page/navigation/navigation.component';
+import { MyOrdersComponent } from './Components/User/my-orders/my-orders.component';
+import { HomePageComponent } from './Components/home/home-page/home-page.component';
+import { LoginComponent } from './Components/User/login/login.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,11 +18,17 @@ import { TutorialComponent } from './components/tutorial/tutorial.component';
     FooterComponent,
     HeaderComponent,
     StayConnectedComponent,
-    TutorialComponent
+    TutorialComponent,
+    NavigationComponent,
+    MyOrdersComponent,
+    HomePageComponent,
+    LoginComponent,
+    routingComponents,
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    AppRoutingModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
